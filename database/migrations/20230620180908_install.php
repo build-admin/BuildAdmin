13396ce2368c49593710cca74c0428579cc1da79
Kyour <kyour@outlook.com>
2024-11-20 16:41:10 +0800

    chore: 移除安装的冗余数据表 test_build

--- database/migrations/20230620180908_install.php
+++ database/migrations/20230620180908_install.php
@@ -367,32 +367,6 @@ class Install extends Migrator
         }
     }
 
-    public function testBuild(): void
-    {
-        if (!$this->hasTable('test_build')) {
-            $table = $this->table('test_build', [
-                'id'          => false,
-                'comment'     => '知识库表',
-                'row_format'  => 'DYNAMIC',
-                'primary_key' => 'id',
-                'collation'   => 'utf8mb4_unicode_ci',
-            ]);
-            $table->addColumn('id', 'integer', ['comment' => 'ID', 'signed' => false, 'identity' => true, 'null' => false])
-                ->addColumn('title', 'string', ['limit' => 100, 'default' => '', 'comment' => '标题', 'null' => false])
-                ->addColumn('keyword_rows', 'string', ['limit' => 100, 'default' => '', 'comment' => '关键词', 'null' => false])
-                ->addColumn('content', 'text', ['null' => true, 'default' => null, 'comment' => '内容'])
-                ->addColumn('views', 'integer', ['comment' => '浏览量', 'default' => 0, 'signed' => false, 'null' => false])
-                ->addColumn('likes', 'integer', ['comment' => '有帮助数', 'default' => 0, 'signed' => false, 'null' => false])
-                ->addColumn('dislikes', 'integer', ['comment' => '无帮助数', 'default' => 0, 'signed' => false, 'null' => false])
-                ->addColumn('note_textarea', 'string', ['limit' => 100, 'default' => '', 'comment' => '备注', 'null' => false])
-                ->addColumn('status', 'enum', ['values' => '0,1', 'default' => '1', 'comment' => '状态:0=隐藏,1=正常', 'null' => false])
-                ->addColumn('weigh', 'integer', ['comment' => '权重', 'default' => 0, 'null' => false])
-                ->addColumn('update_time', 'biginteger', ['signed' => false, 'null' => true, 'default' => null, 'comment' => '更新时间'])
-                ->addColumn('create_time', 'biginteger', ['signed' => false, 'null' => true, 'default' => null, 'comment' => '创建时间'])
-                ->create();
-        }
-    }
-
     public function token(): void
     {
         if (!$this->hasTable('token')) {
